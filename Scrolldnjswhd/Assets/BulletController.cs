65d3d2c84be999d9f30b2c29907ed8e67d4d23e8
dnjswhd98 <88705469+dnjswhd98@users.noreply.github.com>
2021-11-04 17:51:13 +0900

    11041750

---
+++ Scrolldnjswhd/Assets/BulletController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//자동으로 해당 컴퍼넌트 추가
+[RequireComponent(typeof(Rigidbody))]
+public class BulletController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * 2000.0f);
+    }
+}
