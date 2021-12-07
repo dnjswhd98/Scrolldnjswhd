075866f8f9af798f3f24996b8fbd4025fa6de4d0
dnjswhd98 <dnjswhd9803@gmail.com>
2021-12-08 02:55:30 +0900

    12081455

--- HotLine/Assets/Resuorse/Script/JaketMovement/JaketMoving.cs
+++ HotLine/Assets/Resuorse/Script/JaketMovement/JaketMoving.cs
@@ -9,6 +9,8 @@ public class JaketMoving : MonoBehaviour
     [SerializeField]private float Speed;
     [SerializeField]private int WeaponeNum;
     private GameObject Mouse;
+    private float CameraX;
+    private float CameraY;
 
     float angle;
     Vector2 target, mouse;
@@ -24,11 +26,17 @@ public class JaketMoving : MonoBehaviour
         Mouse = GameObject.Find("MouseCursor");
         
         if(Input.GetKey(KeyCode.W))
-        {
-            
-        }
+            transform.Translate(new Vector3(0.0f, 0.1f, 0.0f),Space.World);        
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(new Vector3(0.0f, -0.1f, 0.0f), Space.World);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(new Vector3(-0.1f, 0.0f, 0.0f), Space.World);
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(new Vector3(0.1f, 0.0f, 0.0f), Space.World);
 
         angle = Mathf.Atan2(Mouse.transform.position.y - target.y, Mouse.transform.position.x - target.x) * Mathf.Rad2Deg;
         this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        Camera.main.transform.position = new Vector3(transform.position.x,transform.position.y, Camera.main.transform.position.z);
     }
 }
